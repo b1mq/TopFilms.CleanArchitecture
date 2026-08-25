ef115084c67c17069ed710379f454edcdef49079
Egor <arnoldtagirov1@gmail.com>
2026-08-26 00:03:17 +0200

    created DTO for POCO Object

---
+++ TopFilms.Infrastructure/Services/OmdbMovieDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopFilms.Infrastructure.Services
+{
+    public class OmdbMovieDTO
+    {
+    }
+}
