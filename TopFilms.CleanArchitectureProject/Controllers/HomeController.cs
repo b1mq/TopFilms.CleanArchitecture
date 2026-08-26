0d31e99b91784b04d96f9952f066bf647368a539
Egor <arnoldtagirov1@gmail.com>
2026-08-26 21:58:55 +0200

    Edited Controller

--- TopFilms.CleanArchitectureProject/Controllers/HomeController.cs
+++ TopFilms.CleanArchitectureProject/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TopFilms.Application.Interfaces;
 
 namespace TopFilms.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IFilmManager _filmManager;
+        public HomeController(IFilmManager filmManager)
+        {
+            _filmManager = filmManager;
+        }
+        public async Task<IActionResult> Index()
         {
             return View();
+        }
+        public async Task<IActionResult> GetNewFilm(string title)
+        {
+            var film = await _filmManager.ImportFilmFromApiAsync(title);
+            return View(film);
         }
     }
 }
